5a5616c1ff578ff82ff20d6026cfdede7a1f32a4
waeeelhajji <waelhajji.contact@gmail.com>
2023-12-28 08:17:28 +0100

    Week1Day2

---
+++ Week1/Day1/fundamentals.js
@@ -0,0 +1,48 @@
+// ? ----- SCOPE ----
+
+// var name = "bob"
+
+// function sayHi() {
+//     name = "alice";
+//     console.log(name)
+// }
+// sayHi(); // ?
+// console.log(name) // ?
+
+// ? ----- HOISTING ----
+// console.log("what is x?", x)
+// let x = "bob"
+// const x = "Alice"
+// x = "bob"
+
+
+// const nums = [1, 2, 3, 4]
+
+// [1, 2]
+// 1
+
+// nums[0] = "bob";
+// nums = 1
+// console.log(nums)
+
+// for (let i = 0; i < 5445; i++) {
+
+// }
+// console.log(i)
+
+
+var x = "Alice"
+x[0] = "5"
+console.log(x)
+
+// the output will be "alice"
+// primitive datatypes
+// String
+// Number
+// Boolean
+// undefined / null
+
+
+// complex datatypes
+// Arrays
+// Objects
